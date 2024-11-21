5284262f3459ff00a34f28b399e0ffa42adb5174
oofarhanafarhat <khnkaneez@gmail.com>
2024-11-21 19:15:41 +0500

    Update Footer.tsx
    changes

--- src/app/component/Footer/Footer.tsx
+++ src/app/component/Footer/Footer.tsx
@@ -116,7 +116,7 @@ const Footer = () => {
     
       </div>
       <footer className="place-items-center text-gray-300  ">
-        <h1 className="text-lg mb-5 pb-20">ALL  copyright reserved</h1>
+        <h1 className="text-lg mb-0 pb-20">ALL  copyright reserved</h1>
       </footer>
   
       
